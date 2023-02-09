30d63e026a3d64fea98595b971521395d00a82d6
Joao Paulo <milhomemjoao.work@gmail.com>
2023-02-08 21:41:31 -0300

    Details finalizado

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  color: ${({ theme }) => theme.COLORS.ORANGE};
+  background: none;
+  border: none;
+  font-size: 16px;
+`;
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,53 @@
 import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
 import { Header } from "../../components/header";
-import { Container } from "./styles";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
+import { Container, List, Content } from "./styles";
 
 export function Details() {
   return (
     <Container>
-      <Header/>
-      <Button title="Sair"/>
+      <Header />
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam
+            vitae quod quia odit. Fuga mollitia beatae ex eum vero veritatis
+            ullam voluptatem ad eveniet velit ratione, animi magnam perspiciatis
+            quaerat! Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+            aut iste sint alias iusto. Earum eos harum voluptas expedita
+            molestiae praesentium ipsa nemo! Ut minima alias deserunt porro
+            voluptas nobis?
+          </p>
+
+          <Section title="Links úteis">
+            <List>
+              <li>
+                <a href="">Link 1</a>
+              </li>
+              <li>
+                <a href="">Link 2</a>
+              </li>
+              <li>
+                <a href="">Link 3</a>
+              </li>
+            </List>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="Teste 1" />
+            <Tag title="Teste 2" />
+            <Tag title="Teste 3" />
+          </Section>
+
+          <Button title="Salvar" />
+        </Content>
+      </main>
     </Container>
-  )
+  );
 }
