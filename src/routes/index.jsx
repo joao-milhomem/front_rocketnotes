43ba41a1f53ede7ee5eeccd2d0895104f11e17d3
joao-milhomem <milhomemjoao.work@gmail.com>
2023-02-16 15:27:32 -0300

    redirecionamento de rotas

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
+import { useAuthContext } from "../hooks/auth";
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+  const { user } = useAuthContext();
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      {user ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   );
 }
