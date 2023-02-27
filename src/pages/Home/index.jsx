eae96a806f7a7af25dcc75c2aa54ebc11844ca80
joao-milhomem <milhomemjoao.work@gmail.com>
2023-02-27 10:42:39 -0300

    Cadastro de notas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,8 +5,21 @@ import { FiPlus, FiSearch } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Section } from "../../components/Section";
 import { Note } from "../../components/Note";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 
 export function Home() {
+  const [tags, setTags] = useState();
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags");
+      setTags(response.data);
+    }
+
+    fetchTags();
+  }, []);
+
   return (
     <Container>
       <Brand>
@@ -17,17 +30,15 @@ export function Home() {
 
       <Menu>
         <li>
-          <ButtonText title="Node" />
+          <ButtonText title="Todos" isActive />
         </li>
-        <li>
-          <ButtonText title="React" isActive />
-        </li>
-        <li>
-          <ButtonText title="Express" />
-        </li>
-        <li>
-          <ButtonText title="UseEstate" />
-        </li>
+
+        {tags &&
+          tags.map((tag) => (
+            <li>
+              <ButtonText title={tag.name} key={String(tag.id)} />
+            </li>
+          ))}
       </Menu>
 
       <Search>
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -5,17 +5,68 @@ import { Textarea } from "../../components/Textarea";
 import { Section } from "../../components/Section";
 import { NoteItem } from "../../components/NoteItem";
 import { Button } from "../../components/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { api } from "../../services/api";
 
 export function New() {
+  const navigate = useNavigate();
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState("");
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-  function handleLinks() {
+  function handleAddLinks() {
+    if (newLink == "" || newLink == undefined || newLink == null) {
+      return alert("O campo está vazio");
+    }
     setLinks((prevLinks) => [...prevLinks, newLink]);
-    setNewLink("")
-    console.log(links);
+    setNewLink("");
+  }
+
+  function handleRemoveLinks(deteted) {
+    setLinks(links.filter((link) => link !== deteted));
+  }
+
+  function handleAddTags() {
+    if (newTag == "" || newTag == undefined || newTag == null) {
+      return alert("O campo está vazio");
+    }
+    setTags((prevTags) => [...prevTags, newTag]);
+    setNewTag("");
+  }
+
+  function handleRemoveTags(deleted) {
+    setTags(tags.filter((tag) => tag !== deleted));
+  }
+
+  async function handleAddNote() {
+    if (title == "" || title == undefined || title == null) {
+      return alert("Digite um título para sua nota!");
+    }
+
+    if (description == "" || description == undefined || description == null) {
+      return alert("Digite uma descrição para sua nota!");
+    }
+
+    if (newLink) {
+      return alert("Submeta o link ao formulário!");
+    }
+
+    if (newTag) {
+      return alert("Submeta a tag ao formulário!");
+    }
+    await api.post("/notes", {
+      title,
+      description,
+      links,
+      tags,
+    });
+
+    alert("Nota cadastrada com sucesso!");
+    navigate("/");
   }
 
   return (
@@ -29,13 +80,25 @@ export function New() {
             <Link to="/">Voltar</Link>
           </header>
 
-          <Input placeholder="Título" />
+          <Input
+            placeholder="Título"
+            onChange={(e) => setTitle(e.target.value)}
+          />
 
-          <Textarea placeholder="Observações" />
+          <Textarea
+            placeholder="Observações"
+            onChange={(e) => setDescription(e.target.value)}
+          />
 
           <Section title="Links úteis">
             {links.map((link, index) => (
-              <NoteItem key={index} value={link} onClick={() => {}} />
+              <NoteItem
+                key={index}
+                value={link}
+                onClick={() => {
+                  handleRemoveLinks(link);
+                }}
+              />
             ))}
 
             <NoteItem
@@ -43,18 +106,33 @@ export function New() {
               placeholder="Novo link"
               onChange={(e) => setNewLink(e.target.value)}
               value={newLink}
-              onClick={handleLinks}
+              onClick={handleAddLinks}
             />
           </Section>
 
           <Section title="Marcadores">
             <div className="tags">
-              <NoteItem value="link.com.br" />
-              <NoteItem value="link.com.br" />
-              <NoteItem isNew placeholder="Novo link" />
+              {tags &&
+                tags.map((tag, index) => (
+                  <NoteItem
+                    onClick={() => {
+                      handleRemoveTags(tag);
+                    }}
+                    value={tag}
+                    key={String(index)}
+                  />
+                ))}
+
+              <NoteItem
+                isNew
+                placeholder="Novo link"
+                onChange={(e) => setNewTag(e.target.value)}
+                onClick={handleAddTags}
+                value={newTag}
+              />
             </div>
           </Section>
-          <Button title="Salvar" />
+          <Button title="Salvar" onClick={handleAddNote} />
         </Form>
       </main>
     </Container>
